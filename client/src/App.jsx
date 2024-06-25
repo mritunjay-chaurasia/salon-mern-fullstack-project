86449703ad9cfaa93b7d5eb0b530c08fd47810dc
Mritunjay <you@example.com>
2024-06-26 01:03:32 +0530

    create component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-
+import './App.css'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
           Frontend is working!
-        </p>
-      </header>
     </div>
   );
 }
